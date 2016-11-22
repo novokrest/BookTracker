688adad6d846a1276b18fa169387099ae5d44979
novokrest <novokrest013@gmail.com>
2016-11-22 08:09:05 +0300

    play with bootstrap

---
+++ BookTracker/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using BookTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookTracker.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Book book)
+        {
+            Book? oldBook = Session["book"] as Book?;
+
+            if (oldBook.HasValue)
+            {
+                Votes.ChangeVote(oldBook.Value, book);
+            }
+            else
+            {
+                Votes.RegisterVote(book);
+            }
+
+            ViewBag.SelectedBook = Session["book"] = book;
+            return View();
+        }
+    }
+}
---
+++ BookTracker/Models/Votes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookTracker.Models
+{
+    public enum Book
+    {
+        ProAspNetMvcPlatform,
+        PatterntOfEnterpriseApplication,
+        AndroidForProfessional
+    }
+
+    public class Votes
+    {
+        private readonly static IDictionary<Book, int> _votes = new Dictionary<Book, int>();
+
+        public static int GetVotes(Book book)
+        {
+            return _votes.ContainsKey(book) ? _votes[book] : 0;
+        }
+
+        public static void RegisterVote(Book book)
+        {
+            _votes[book] = _votes.ContainsKey(book) ? ++_votes[book] : 1;
+        }
+
+        public static void ChangeVote(Book oldBook, Book newBook)
+        {
+            if (_votes.ContainsKey(oldBook))
+            {
+                --_votes[oldBook];
+            }
+            RegisterVote(newBook);
+        }
+    }
+}
